93a96fef5641254108ad91e6e5a56094e0907206
RoxanneLefebvre <102246244+RoxanneLefebvre@users.noreply.github.com>
2023-01-25 15:18:07 -0500

    ajout de deux tables, etudiants et villes dans migrations

---
+++ database/migrations/2023_01_25_201358_etudiants.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Etudiants extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('blog_posts', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 100);
+            $table->text('body');
+            $table->integer('user_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
